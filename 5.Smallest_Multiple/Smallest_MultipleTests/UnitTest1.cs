e6f348619b762140da66a157d7eacf923a5fdf10
Sean Robinson <Sean.T.Robinson@googlemail.com>
2014-06-16 21:29:50 +0100

    Problem 6 Added

---
+++ 5.Smallest_Multiple/Smallest_MultipleTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Smallest_MultipleTests
+{
+    [TestClass]
+    public class SmallestMultipleTestClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 6.Sum_Square_Difference/6.Sum_Square_Difference/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//25164150
+
+namespace _6.Sum_Square_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateDiff(100);
+        }
+
+        private static void CalculateDiff(int limit)
+        {
+            int sum = 0, sumOfSquares = 0;
+            for (int i = 1; i <= limit; i++)
+            {
+                sum += i;
+                sumOfSquares += (i * i);
+            }
+
+            int squareOfSum = (sum * sum);
+            int diff = (squareOfSum - sumOfSquares);
+            Console.WriteLine("Difference between {0} and {1}: {2}", squareOfSum, sumOfSquares, diff);
+        }
+    }
+}
